37638014283e0dffca0fcfa7308294da683888dc
Harley Ehrman <harley.ehrman@outlook.com>
2018-05-29 21:49:24 -0400

    Add project files.

---
+++ Multiplication/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Multiplication
+{
+    partial class MultiTable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(25, 56);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(262, 25);
+            this.lblInput.TabIndex = 0;
+            this.lblInput.Text = "Enter a number to multiply";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(293, 53);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(66, 31);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(236, 121);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(122, 51);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(30, 374);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(199, 51);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit Application";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.ForeColor = System.Drawing.Color.Maroon;
+            this.lblOutput.Location = new System.Drawing.Point(459, 59);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(78, 25);
+            this.lblOutput.TabIndex = 4;
+            this.lblOutput.Text = "output";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(87, 121);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(122, 51);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // MultiTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 449);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblInput);
+            this.Name = "MultiTable";
+            this.Text = "Multiplication";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Multiplication/Form1.cs
@@ -0,0 +1,73 @@
+﻿/********************************************************************
+ * Assignment from SDEV 240                                         *
+ * Coded by: Harley Ehrman                                          *
+ *                                                                  *
+ * From book:                                                       *
+ * Microsoft Visual C# 2015:                                        *
+ * An Introduction to Object-Oriented Programming 6th Edition       *
+ * by Joyce Farrell                                                 *
+ *                                                                  *
+ * Instructions (Page 307, Exercise 6):                             *
+ * Create an application named Multiplication whose Main() method   *
+ * the user to input an integer and then calls a method named       *
+ * DisplayMultiplicationTable(), which displays the results of      *
+ * multiplying the integer by each of the numbers 2 through 10.     *
+ ********************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiplication
+{
+    public partial class MultiTable : Form
+    {
+        public MultiTable()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            double multi;
+
+            //set if to validate for numbers only in input
+            if (double.TryParse(txtInput.Text, out multi))
+            {
+                //declare and assign variable
+                multi = Convert.ToInt32(txtInput.Text);
+
+                //call DisplaMultiplicationTable method
+                DisplaMultiplicationTable(multi);
+            }
+            else
+            {
+                lblOutput.Text = "ERROR!" + "\n" + "You must" + "\n" + "enter a" + "\n" + "number.";
+            }  
+        }
+        private void DisplaMultiplicationTable(double value)
+        {
+            
+            //for loop
+            for (double x = 2; x <= 10; ++x)
+            {
+                lblOutput.Text += Convert.ToString(value * x) + "\n";
+            }
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblOutput.ResetText();
+            txtInput.ResetText();
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
